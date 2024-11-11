3660dd6c208968034266018e720f14973ddd34ce
seoninja13 <dachevivo@gmail.com>
2024-11-10 21:14:50 -0800

    Update environment.ts
    shanged the config some dummy data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyA_UBJafV8LnDbtpP4SxT-0GKy6s75mEbk",
+    apiKey: "AIzaSyA_UBJafV8Ln9DbtpP4S75xT-0GKy6s75mEbk",
     authDomain: "jobber-clone.firebaseapp.com",
     projectId: "jobber-clone",
     storageBucket: "jobber-clone.firebasestorage.app",
     messagingSenderId: "489497407222",
-    appId: "1:489497407222:web:3f67b22097b10084cb46d7"
+    appId: "1:489497407222:web:3f67b22097b1009184cb46d7"
   },
   sendgrid: {
     apiKey: ''
