6bba55fa9888f0cdc0142bd421df6847f1e5a369
seoninja13 <dachevivo@gmail.com>
2024-11-10 21:15:25 -0800

    Update environment.prod.ts
    added dummy numbers

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA_UBJafV8LnDbtpP4SxT-0GKy6s75mEbk",
+    apiKey: "AIzaSyA_UBJafV80LnDbtpP4SxT-0GKy6s75mEbk",
     authDomain: "jobber-clone.firebaseapp.com",
     projectId: "jobber-clone",
     storageBucket: "jobber-clone.firebasestorage.app",
     messagingSenderId: "489497407222",
-    appId: "1:489497407222:web:3f67b22097b10084cb46d7"
+    appId: "1:489497407222:web:3f67b220977b10084cb46d7"
   },
   sendgrid: {
     apiKey: ''
